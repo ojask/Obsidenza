881ab7978b595a55de141752a152301ec1b29033
ojask <techno.mind69@gmail.com>
2023-07-23 20:54:26 +0530

    e

--- Obsidian/categories/do.sh
+++
@@ -1,8 +0,0 @@
-for size in *; do
-if [ -f "../apps/$size/webapp-manager.svg ]; then
-ln -s ../../apps/$size/webapp-manager.svg $size/webapp-manager.svg
-fi
-if [ -f "../apps/$size/webapp-manager.png ]; then
-ln -s ../../apps/$size/webapp-manager.png $size/webapp-manager.png
-fi
-done
